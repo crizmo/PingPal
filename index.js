95b69fff802c039aa133ce9b66a4b7c96221cb95
Kurizu <83665497+crizmo@users.noreply.github.com>
2023-07-10 10:34:03 +0530

    🔥 ES

--- index.js
+++ index.js
@@ -1,55 +1,54 @@
-import fetch from "node-fetch";
-import express from "express";
+const express = require("express");
+const axios = require("axios");
 
 const app = express();
 const port = 3000;
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
-});
+  });  
 
 // Array to store the URLs
 const urls = [
-    "https://discord-cards.kurizu.repl.co/api/compact/784141856426033233",
-    // Add more URLs here
+  "https://discord-cards.kurizu.repl.co/api/compact/784141856426033233",
+  // Add more URLs here
 ];
 
 const fetchData = async (url) => {
-    try {
-        const response = await fetch(url);
-        const status = response.status;
-        return status;
-    } catch (error) {
-        console.error(`Error fetching data from ${url}:`, error);
-        return console.error;
-    }
+  try {
+    const response = await axios.get(url);
+    const status = response.status;
+    return status;
+  } catch (error) {
+    console.error(`Error fetching data from ${url}:`, error);
+    return console.error;
+  }
 };
 
 const keepReplAlive = () => {
-    // Fetch data for each URL
-    urls.forEach((url) => {
-        fetchData(url);
-    });
+  // Fetch data for each URL
+  urls.forEach((url) => {
+    fetchData(url);
+  });
 };
 
 setInterval(keepReplAlive, 25 * 60 * 1000); // 25 minutes
 
 app.get("/", (req, res) => {
-    res.send("Server is running");
+  res.send("Server is running");
 });
 
 app.get("/status", async (req, res) => {
-    const statusList = {};
-    for (const url of urls) {
-        const status = await fetchData(url);
-        statusList[url] = status;
-    }
-    res.json(statusList);
+  const statusList = {};
+  for (const url of urls) {
+    const status = await fetchData(url);
+    statusList[url] = status;
+  }
+  res.json(statusList);
 });
 
 app.get("/keepalive", (req, res) => {
-    res.send("Keep-alive is working!");
+  res.send("Keep-alive is working!");
 });
 
-// export the app for testing
-export default app;
+module.exports = app;
